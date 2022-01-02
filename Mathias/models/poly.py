remove legend error, make scaling in correct order

Mathias/models/poly.py
@@ -187,7 +187,7 @@ class PolyModel(pl.LightningModule):
                 ax[0].set_title("Learned Exponent Paths")
                 ax[0].set_xlabel("Epoch")
                 ax[0].set_ylabel("Exponent Value")
-                ax[0].legend()
+
 
                 # Coefficients
                 for i in range(coefficient_path.shape[-1]):
@@ -196,8 +196,7 @@ class PolyModel(pl.LightningModule):
                 ax[1].set_title("Learned Coefficient Paths")
                 ax[1].set_xlabel("Epoch")
                 ax[1].set_ylabel("Coefficient Value")
-                ax[1].legend()
-            
+        
             else:
                 # Sort from lowest to largest exponent
                 ind = np.argsort(exponent_path[-1])

Mathias/plotter.py
@@ -35,6 +35,7 @@ class predictions:
         else:
             self.X_test  = self.dm.X_test
             self.y_test = self.dm.y_test
+
             
     def plot(self):
         # clear all plots
@@ -44,26 +45,28 @@ class predictions:
         # sns.set_style("darkgrid")
 
         with torch.no_grad():
-            self.y_pred_test = self.model(self.X_test.to(torch.device(self.device)))
-            self.y_pred_train = self.model(self.X_train.to(torch.device(self.device)))
+            y_pred_test = self.model(self.X_test.to(torch.device(self.device)))
+            y_pred_train = self.model(self.X_train.to(torch.device(self.device)))
 
         if (self.show_orig_scale and self.scale):
             # transform back to original scale before plotting
-            self.X_train = self.dm.scaler.inverse_transform(self.X_train)
-            self.X_train = torch.from_numpy(self.X_train).to(torch.float32)
-            self.X_test = self.dm.scaler.inverse_transform(self.X_test)
-            self.X_test = torch.from_numpy(self.X_test).to(torch.float32)
+            X_train = self.dm.scaler.inverse_transform(self.X_train)
+            X_train = torch.from_numpy(X_train).to(torch.float32)
+            X_test = self.dm.scaler.inverse_transform(self.X_test)
+            X_test = torch.from_numpy(X_test).to(torch.float32)
+        else:
+            X_train = self.X_train
+            X_test = self.X_test
 
-        x = torch.cat([self.X_train, self.X_test], dim=0)
+        x = torch.cat([X_train, X_test], dim=0)
         x = torch.from_numpy(np.linspace(x.min(), x.max(), 200)).view(-1, 1) # linearf/ polynf expect this shape for .sum()
 
         # Create groundtruth over possibly larger domain
         y = self.target_fn(x)
-        
 
         plt.plot(x, y, label="groundtruth")
-        plt.scatter(self.X_train, self.y_pred_train.cpu(), c="orange", alpha=1., label="train prediction", marker=".", zorder=10.) #zorder defines which points have priority
-        plt.scatter(self.X_test, self.y_pred_test.cpu(), c="green", alpha=0.5, label="test prediction", marker=".", zorder=5.)
+        plt.scatter(X_train, y_pred_train.cpu(), c="orange", alpha=1., label="train prediction", marker=".", zorder=10.) #zorder defines which points have priority
+        plt.scatter(X_test, y_pred_test.cpu(), c="green", alpha=0.5, label="test prediction", marker=".", zorder=5.)
         plt.xlabel("x")
         plt.ylabel("y")
         plt.legend()